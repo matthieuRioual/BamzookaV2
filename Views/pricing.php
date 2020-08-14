c6fce5cf2ab4542e1c2cb5483b20b31fc1923c80
RIOUAL Matthieu <60732084+matthieulapatate@users.noreply.github.com>
2020-08-14 14:36:43 +0200

    pricingpage

--- Views/pricing.php
+++ Views/pricing.php
@@ -9,7 +9,7 @@
               <h4>Change at any time</h4>
             </div>
             <div class="row justify-content-md-center">
-                <div class="col-md-5 col-lg-4">
+                <div class="col-md-6 col-lg-4">
                     <div class="item">
                         <div class="heading">
                             <h3>Standart</h3>
@@ -26,22 +26,29 @@
                         <button class="btn btn-block btn-outline-primary" type="submit">Try for free</button>
                     </div>
                 </div>
-                <div class="col-md-5 col-lg-4">
+                <div class="col-md-6 col-lg-4">
                     <div class="item">
                         <div class="ribbon">50% off for <br> non profit</div>
                         <div class="heading">
                             <h3>Business</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <div class="features">
-                            <h4><i class="far fa-check-circle mr-2"></i><span class="feature">Full Support</span> : <span class="value">Yes</span></h4>
-                            <h4><span class="feature">Duration</span> : <span class="value">60 Days</span></h4>
-                            <h4><span class="feature">Storage</span> : <span class="value">50GB</span></h4>
+                        <div class="features justify-content-center">
+                            <ul>
+                                <li class="nonpoint"><i class="far fa-check-circle mr-2"></i>Conditional logic</li>
+                                <li class="nonpoint"><i class="far fa-check-circle mr-2"></i>Calendar view</li>
+                                <li class="nonpoint"><i class="far fa-check-circle mr-2"></i>Enforced task order</li>
+                                <li class="nonpoint"><i class="far fa-check-circle mr-2"></i>User input validation with pass/fail</li>
+                                <li class="nonpoint"><i class="far fa-check-circle mr-2"></i>Email & phone support with TX staff</li>
+                                <li class="nonpoint"><i class="far fa-check-circle mr-2"></i>99.9% uptime SLA</li>
+                                <li class="nonpoint"><i class="far fa-check-circle mr-2"></i>Dedicated customer success manager</li>
+                                <li class="nonpoint"><i class="far fa-check-circle mr-2"></i>Flowers once a week</li>
+                            </ul>
                         </div>
                         <div class="price">
                             <h4>$50</h4>
                         </div>
-                        <button class="btn btn-block btn-primary" type="submit">BUY NOW</button>
+                        <button class="btn btn-block btn-primary" type="submit">Try it for free</button>
                     </div>
                 </div>
                 
